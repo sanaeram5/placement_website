83ff1988d7191d0daab4bf95db616b7d21d168eb
Saurabh Dwivedi <sbh.dwivedi07@gmail.com>
2021-06-30 15:25:58 +0530

    faculty

--- src/components/Faculty.js
+++ src/components/Faculty.js
@@ -12,24 +12,26 @@ class Faculty extends React.Component {
       <div class="container">
           <div className="faculty-heading">FACULTY PROFILE</div>
           <div className="row mt-4">
-            <div className="col-4">
+            {Data.map(res=>(
+           <div className="col-4">
             <div class="card m-2 faculty-card">
-                {/* <img src={image_quadri} className="faculty-img rounded-circle" alt=""/> */}
+                <img src={res.image} className="faculty-img rounded-circle" alt=""/>
                 <div className="card-body">
-                <div className="faculty-name">DR. SMK QUADRI</div>
-                <div className="faculty-designation">Professor and Head of Department (M.Tech, PhD)</div>
-                <div className="faculty-exp">Year of Experience: 29</div>
+                <div className="faculty-name">{res["faculty-name"]}</div>
+                <div className="faculty-designation">{res["faculty-designation"]}</div>
+                <div className="faculty-exp">{res["faculty-exp"]}</div>
                 <div className="faculty-specialization">Areas of Specialization :</div>
-                <div className="spacial-in">Software Reliablit engineering,Software testing and Software engineering</div>
+                <div className="spacial-in">{res["faculty-exp"]}</div>
             </div>
             </div>
-          </div>
+          </div>))}
 </div>
 
 
 
       </div>
     </div>
+
   );
 };
 }
