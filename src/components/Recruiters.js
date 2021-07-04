51a7b2b696231357889c1d9bc1f68f8f4a6d08a5
Saurabh Dwivedi <sbh.dwivedi07@gmail.com>
2021-07-04 08:46:27 +0530

    Recruiters Completed

--- src/components/Recruiters.js
+++ src/components/Recruiters.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Myloop from './images';
 import './Recruiters.css';
 import data from './data';
+import Header from './Header';
 
 class Recruiters extends React.Component{
     render(){
 
         return(
             <div className="recruiters-main">
+                <Header/>
                 <div className="heading">
                     <p>❖</p>
                     <p>PAST RECRUITERS</p>
@@ -20,9 +22,9 @@ class Recruiters extends React.Component{
                 <div>
                     <div className="container-past">
                     <div className="past-placements">
-                        <p>❖</p>
+                        <p>✣</p>
                         <p>OUR PAST PLACEMENTS</p>
-                        <p>❖</p>
+                        <p>✣</p>
                     </div>
                 </div>
                
