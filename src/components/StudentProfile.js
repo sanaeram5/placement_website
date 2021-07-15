8f588afc0e40914739015df7ff1a3b67a88f8bae
SANA ERAM <sanaeram5@gmail.com>
2021-07-15 14:03:06 +0530

    new version 1.0

--- src/components/StudentProfile.js
+++ src/components/StudentProfile.js
@@ -2,49 +2,14 @@ import React, {useState} from "react";
 import  "./StudentProfile.css";
 import Header from './Header';
 import PageHeader from './PageHeader';
-import { Container, Modal, Button, Card } from "react-bootstrap";
+import { Container, Modal, Button, Card, Row } from "react-bootstrap";
 import jpic from "./../web_design/student_page/students_pics/JaanbaazAkhtar.jpg";
-
-function MyVerticallyCenteredModal(props) {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-
-
-
-
+import Profile from './StudentProfileCard';
 
 const StudentProfile = () => {
 
-    const [modalShow, setModalShow] = useState(false);
-
     return(
         <>
-            <Header/>
-
             <section className="container">
                 <PageHeader page="Student Profile"/>
             </section>
@@ -63,38 +28,76 @@ const StudentProfile = () => {
                         <div id="home" class="tab-pane fade in active">
                             <h3>HOME</h3>
 
-                                <Card className="studentCard" onClick={() => setModalShow(true)} style={{ width: '13rem', borderRadius:'20px',textAlign:'center', alignItems:'center' }}>
-                                    <Card.Img variant="top" className="m-3" src={jpic}style={{width:'85%', borderRadius:'15px'}}  />
-                                    <Card.Body>
-                                        <Card.Title>Jaanbaaz Akhtar</Card.Title>
-                                        <Card.Text>
-                                            Mca 4th sem
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-
-
-                                <MyVerticallyCenteredModal
-                                    show={modalShow}
-                                    onHide={() => setModalShow(false)}
-                                />
-
+                            <Row>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                            </Row>
+                            <Row>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                            </Row>
+                            <Row>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                            </Row>
+                            <Row>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                            </Row>
+                            <Row>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                            </Row>
+                            <Row>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                            </Row>
+                            <Row>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                            </Row>
+                            <Row>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                            </Row>
+                            <Row>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                            </Row>
+                            <Row>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                            </Row>
                         </div>
 
                         <div id="menu1" class="tab-pane fade">
 
                             <h3>Menu 1</h3>
 
-                                <Card className="studentCard" onClick={() => setModalShow(true)} style={{ width: '13rem', borderRadius:'20px',textAlign:'center', alignItems:'center' }}>
-                                    <Card.Img variant="top" className="m-3" src={jpic}style={{width:'85%', borderRadius:'15px'}}  />
-                                    <Card.Body>
-                                        <Card.Title>Jaanbaaz Akhtar</Card.Title>
-                                        <Card.Text>
-                                            Mca 4th sem
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
+                                <Profile title="Jaanbaaz Akhtar" text="Mca 4th sem" image={jpic}/>
                         </div>
                     </div>
                 </Container>
