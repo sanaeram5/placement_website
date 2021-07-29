ebb507603a5cf93f779773de2270ca48e143bc4c
Azim <aazim5215@gmail.com>
2021-07-29 19:59:36 +0530

    student database added

--- src/index.js
+++ src/index.js
@@ -6,19 +6,13 @@ import {BrowserRouter as Router,Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-<<<<<<< HEAD
+
 // import ScrollToTop from './components/scrollToTop';
-=======
->>>>>>> 2046840a5651c0db881f2918a60ccb9785bc3cc0
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-<<<<<<< HEAD
     {/* <ScrollToTop/> */}
-    
-=======
->>>>>>> 2046840a5651c0db881f2918a60ccb9785bc3cc0
     <App />
     <Footer/>
     </Router>
